763c48a10854524da9a6663a5b37aa70ef0a07ef
sugiyama404 <mmingriha@gmail.com>
2021-04-28 15:20:44 +0900

    ok

--- opt/main.py
+++ opt/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+import time
 clf = linear_model.LinearRegression()
 
 gdp = pd.read_csv('import/GDP_JAPAN.csv',
@@ -66,11 +67,69 @@ newdf2 = newdf2.drop('IIP_YOY_Q1', axis=1)
 newdf2 = newdf2.drop('IIP_YOY_Q2', axis=1)
 newdf2 = newdf2.drop('IIP_YOY_Q3', axis=1)
 
+# ブリッジ方程式モデル(単回帰分析)
+df4 = pd.merge(gdp, iip, left_index=True, right_index=True, how='outer')
+df5 = pd.DataFrame(columns=['BRIDGE'])
+
+x = np.array([])
+flag = False
+for date, IIP_YOY in zip(df4.index, df4['IIP_YOY']):
+
+    x = np.append(x, IIP_YOY)
+    if flag == False:
+        if date == '2013-07-01':
+            flag = True
+
+    if flag:
+        x3t = x[-1]
+        x3tm1 = x[-2]
+        x3tm2 = x[-3]
+        x3tm3 = x[-4]
+        x3tm4 = x[-5]
+        xt = (x3t + 2*x3tm1 + 3*x3tm2 + 2*x3tm3 + x3tm4)/3
+        record = pd.Series([xt],
+                           index=df5.columns, name=date)
+        df5 = df5.append(record)
+
+df5.index.name = 'DATE'
+df6 = pd.merge(df4, df5, left_index=True, right_index=True, how='outer')
+df6 = df6.dropna()
+
+# 目的変数(Y)、説明変数(X)
+Y2 = df6['GDP_CYOY']
+X2 = df6[['BRIDGE']]
+
+# 線形回帰をする
+model = linear_model.LinearRegression()
+model.fit(X2, Y2)
+
+# パラメータ算出
+reg_a = model.coef_[0]
+reg_b = model.intercept_
+
+df6['NOWCAST'] = df6.apply(lambda x: reg_b + reg_a*x['BRIDGE'], axis=1)
+
+newdf3 = df6.copy()
+newdf3 = newdf3.drop('IIP_YOY', axis=1)
+newdf3 = newdf3.drop('BRIDGE', axis=1)
+
 if __name__ == '__main__':
     print("ok")
-    #pd.set_option('display.max_columns', 4)
-    # print(df)
+    '''
     plt.figure()
     newdf2.plot()
     plt.savefig('export/nowcaste.png')
+    plt.close('all')
+    '''
+    # Plot the sample data
+    '''
+    df3 = pd.merge(gdp, iip, left_index=True, right_index=True, how='outer')
+    plt.figure()
+    df3.plot(figsize=(15, 4), style=['o', '-'])
+    plt.savefig('export/gdpandiip.png')
+    plt.close('all')
+    '''
+    plt.figure()
+    newdf3.plot()
+    plt.savefig('export/nowcaste_bridge.png')
     plt.close('all')
